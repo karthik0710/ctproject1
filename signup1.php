91c0d0ba5a069ad863b5b0e40c21070652017a69
karthik0710 <ms.jaganhari@gmail.com>
2018-04-04 10:22:41 +0530

    Create signup1.php

---
+++ signup1.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if(isset($_POST['submit']))
+{
+    $sql = "INSERT INTO users (username, password, email)
+    VALUES ('".$_POST["email"]."','".$_POST["password"]."','".$_POST["phn"]."')";
+
+    $result = mysqli_query($conn,$sql);
+}
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
